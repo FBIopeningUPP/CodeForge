6f1c7dff4b112152e2f45563799bf72e60cd0abf
Milind Singh <milindsingh486@gmail.com>
2026-07-11 09:57:21 +0530

    added the things to store js file

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,36 @@
 import { create } from 'zustand'
+import { UPGRADES, getUpgradeCost } from './upgrades'
 
 export const useStore = create((set) => ({
     loc: 0,
     locPerSec: 0,
     clickPower: 1,
 
+    owned: Object.fromEntries(UPGRADES.map(u => [u.id, 0])),
+
     click: () => set((state) => ({loc: state.loc + state.clickPower})),
+
     addAutoLoc: (amount) => set((state) => ({loc: state.loc + amount})),
+
+    buyUpgrade: (id) => {
+        const state = get()
+        const upgrade = UPGRADES.find(u => u.id === id)
+        if(!upgrade) return
+
+        const count = state.owned[id]
+        const cost = getUpgradeCost(upgrade, count)
+
+        if (state.loc < cost) return
+
+        const newOwned = { ...state.owned, [id]: count + 1 }
+        const newLocPerSec = UPGRADES.reduce((total, u) => {
+            return total + u.baseProduction * newOwned[u.id]
+        }, 0)
+
+        set({
+            loc: state.loc - cost,
+            owned: newOwned,
+            locPerSec: newLocPerSec,
+        })
+    },
 }))
